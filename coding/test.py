8ae4975e2aa5e8233bd784259107d97a446bf7d5
Lei Wang <wanglei.okay@gmail.com>
2022-06-09 18:17:03 +0300

    PDS_AWS_ADT: C1_w1

--- coding/test.py
+++ coding/test.py
@@ -1,20 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        n= len(s1)
+        n = len(s1)
         m = len(s2)
 
         for i in range(n-m):
             print(s1[i:i+m])
             if set(s1[i:i+m]) == set(s2):
                 return True
-        
+
         return False
 
+
 class Node:
-    def __init__(self,cargo = None, next = None):
+    def __init__(self, cargo=None, next=None):
         self.cargo = cargo
         self.next = next
+
     def __str__(self):
-        #测试基本功能，输出字符串
+        # 测试基本功能，输出字符串
         return str(self.cargo)
---
+++ tbd/questions.md
@@ -0,0 +1,28 @@
+# Questions in Daily Develloping
+
+## Linux 
+
+* Kernel: SIGSEGV (Segmentation Fault)
+
+* Kernel OOM-Killer
+
+## Python
+
+* `__evaluated__`
+
+* `__main__` model
+
+* Python Shell (REPL)
+
+* Joblib Model
+
+* Staticmethod 
+
+* Laziness
+
+* callable 
+
+* Python Bound Methods  "Pickle Module"
+
+* RPC Result
+
